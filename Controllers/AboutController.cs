2294a5bb64aee16a603367bc6a113d8099ac2f3a
to0sya <anatolii.kochan.pz.2021@lpnu.ua>
2024-05-17 19:19:47 +0300

    [feat] Add routing logic. Add controllers for routes.

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace url_shortener.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ShortUrlController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace url_shortener.Controllers
+{
+    public class ShortUrlController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
